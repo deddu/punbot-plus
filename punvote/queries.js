be6b9d2af22c5b96d0c77e7fe2d57f1e5b835316
andrea cadeddu <cadedduan@gmail.com>
2019-12-30 10:15:54 -0600

    queries seems allright now

--- punvote/queries.js
+++ punvote/queries.js
@@ -5,21 +5,6 @@ const VOTESSTABLE = process.env.VOTES_TABLE_NAME || "votesstable"
 const MONTHLY_SCORES_TABLE = process.env.MONTHLY_SCORES_TABLE_NAME || "monthlyscoresstable"
 const LIFE_SCORES_TABLE = process.env.LIFE_SCORES_TABLE_NAME || "lifescoresstable"
 
-
-const get_scores = (chan)=>{
-    const params = {
-        TableName: TABLE,
-        Key: {chan:chan}
-    }
-    return documentClient.get(params)
-        .promise()
-        .then( x => x.Item)
-        .catch(e => {
-            console.error(e);
-            return {}
-        })
-}
-
 function getMsg (chan_author, msg_id){
     const params = {
         TableName: VOTESTABLE,
@@ -48,7 +33,26 @@ function top10Ever (chan){
     }
     return documentClient.query(params)
         .promise()
-        .then( x => x.Item)
+        .then( x => x.Items)
+        .catch(e => {
+            console.error(e);
+            return {}
+        })
+}
+
+function shittiestEver(chan){
+    const params = {
+        TableName: VOTESTABLE,
+        IndexName: "Top10Ever",
+        Limit:1,
+        KeyConditionExpression: 'pk = :hkey',
+        ExpressionAttributeValues: {
+          ':hkey': chan,
+        }
+    }
+    return documentClient.query(params)
+        .promise()
+        .then( x => x.Items)
         .catch(e => {
             console.error(e);
             return {}
@@ -68,7 +72,26 @@ function top10Author (author){
     }
     return documentClient.query(params)
         .promise()
-        .then( x => x.Item)
+        .then( x => x.Items)
+        .catch(e => {
+            console.error(e);
+            return {}
+        })
+}
+
+function shittiestAuthor (author){
+    const params = {
+        TableName: VOTESTABLE,
+        IndexName: "AuthorsTop10",
+        Limit:1,
+        KeyConditionExpression: 'pk = :hkey',
+        ExpressionAttributeValues: {
+          ':hkey': 'key',
+        }
+    }
+    return documentClient.query(params)
+        .promise()
+        .then( x => x.Items)
         .catch(e => {
             console.error(e);
             return {}
@@ -88,6 +111,39 @@ function top10Month (chan, yymm){
     }
     return documentClient.query(params)
         .promise()
+        .then( x => x.Items)
+        .catch(e => {
+            console.error(e);
+            return {}
+        })
+}
+
+function shittiestMonth (chan, yymm){
+    const params = {
+        TableName: VOTESTABLE,
+        IndexName: "Top10month",
+        Limit:1,
+        KeyConditionExpression: 'pk = :hkey',
+        ExpressionAttributeValues: {
+          ':hkey': `${chan}:${yymm}`,
+        }
+    }
+    return documentClient.query(params)
+        .promise()
+        .then( x => x.Items)
+        .catch(e => {
+            console.error(e);
+            return {}
+        })
+}
+
+function authorsRankMonth (chan, yymm){
+    const params = {
+        TableName: MONTHLY_SCORES_TABLE,
+        Key:{ pk:`${chan}:${yymm}`}
+    }
+    return documentClient.get(params)
+        .promise()
         .then( x => x.Item)
         .catch(e => {
             console.error(e);
@@ -95,15 +151,29 @@ function top10Month (chan, yymm){
         })
 }
 
-//TODO: those will be queries, should return promises
-module.export = {
-    getMsg:getMsg
+function authorsRankEver (chan){
+    const params = {
+        TableName: MONTHLY_SCORES_TABLE,
+        Key:{ pk:chan}
+    }
+    return documentClient.get(params)
+        .promise()
+        .then( x => x.Item)
+        .catch(e => {
+            console.error(e);
+            return {}
+        })
 }
-export const top10 =()=>"top10"
-export const top10Month =(month='current')=>"top10month-"+month
-export const top10author = (author) => "top10author-"+author
-export const shittiest =()=>"shittiest"
-export const shittiestMonth =(month='current')=>"shittiestMonth-"+month
-export const shittiestAuthor = (author) => "shittiestAuthor-"+author
-export const authorsRankMonth = (month='current')=>"rankmonth"+month
-export const authorsRankEver = ()=>"all time author ranks"
+
+
+module.export = {
+    getMsg,
+    top10Ever,
+    top10Author,
+    top10Month,
+    shittiestEver,
+    shittiestAuthor,
+    shittiestMonth,
+    authorsRankEver,
+    authorsRankMonth
+}
