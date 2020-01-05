4b38500639d240e0c80542bda8ed752575afd6ad
andrea cadeddu <cadedduan@gmail.com>
2020-01-05 10:13:59 -0600

    tests and ever aggregating functions

--- punsmonthly/index.js
+++ punsmonthly/index.js
@@ -8,9 +8,9 @@ const log = require('debug')('monthly:index');
 const newItemLens = R.lensPath(['dynamodb','NewImage'])
 const oldItemLens = R.lensPath(['dynamodb','OldImage'])
 
-const compute_score = (author_aggregate) => {
-    const keys = Object.keys(author_aggregate.punsScores)
-    const tot =  keys.reduce((acc,x) => author_aggregate.punsScores[x]+ acc, 0)
+function compute_score(author_aggregate,field='punsScores') {
+    const keys = Object.keys(author_aggregate[field])
+    const tot =  keys.reduce((acc,x) => author_aggregate[field][x]+ acc, 0)
     return tot / (keys.length)
 }
 
@@ -28,61 +28,33 @@ const getOld = (x) => AWS.DynamoDB.Converter.unmarshall(R.view(oldItemLens, x))
 //        }
 //      }
 async function process_record(r){
-    let current_agg 
+    let current_agg, life_agg
     switch (r.eventName){
         case 'INSERT':
         case 'MODIFY':{
             const updated_pun = getNew(r)
             //so we have the new pun. We go grab this fella's record for this chan
-            
-            const [chan,author] = updated_pun.pk.split(':');
-            const score = updated_pun.score;
-            const punId = updated_pun.sk;
             current_agg = await q.getItem(updated_pun.chan_yymm);
-            log('found current', current_agg);
-
-            if (!current_agg){
-                //not existing, we initialize it
-                current_agg = {
-                    pk:updated_pun.chan_yymm,
-                    authors:{
-                        [author]:{
-                            avg:0,
-                            punsScores:{}
-                        }
-                    }
-                }
-            }
-            
-            // updates pun score
-            current_agg.authors[author].punsScores[punId]=score;
-            // updates avg
-            current_agg.authors[author].avg = compute_score(current_agg.authors[author]);
-            log('updating with', current_agg);
-            await mut.put_record(current_agg);
+            life_agg = await q.getItem(updated_pun.chan);
+            log('found current', current_agg,life_agg);
+            current_agg = upMonthly(current_agg, updated_pun);
+            life_agg = upEver(life_agg, current_agg, updated_pun);
             break;
         }
         case 'REMOVE':{
             const deleted_pun = getOld(r)
             log('updated pun', deleted_pun);
-            const [chan,author] = deleted_pun.pk.split(':');
-            const punId = deleted_pun.sk;
             current_agg = await q.getItem(deleted_pun.chan_yymm);
-            log('found current', current_agg);
-            delete current_agg.authors[author].punsScores[punId]
-            if (Object.keys(current_agg.authors[author].punsScores)>0){
-                //still has puns to score
-                current_agg.authors[author].avg = compute_score(current_agg.authors[author]);
-            }
-            else {
-                //doesn't have any more puns, let's give him a nice 0
-                current_agg.authors[author].avg = 0
-            }
-            log('updating with', current_agg);
-            await mut.put_record(current_agg);
+            life_agg = await q.getItem(deleted_pun.chan);
+            log('found current', current_agg, life_agg);
+            current_agg = rmMonthly(current_agg, deleted_pun);
+            life_agg = upEver(life_agg,current_agg,deleted_pun);
             break;
         }
     }
+    log('updating with', current_agg,life_agg);
+    await mut.put_record(current_agg);
+    await mut.put_record(life_agg);
     return current_agg;
 }  
 
@@ -93,13 +65,104 @@ exports.handler = async (event) => {
         log("processing", r)
         await process_record(r)
     }
-        
-    
     return event;
 };
+
+// {
+//        pk: chan_yymm
+//        authors : {
+//          author_id:{
+//            avg:number,
+//            punsScores: { punId:number }
+//            }
+//        }
+//      }
+function upMonthly(current_agg, newPun) {
+    const [chan,author] = newPun.pk.split(':');
+    const score = newPun.score;
+    const punId = newPun.sk;
+    if (!current_agg) {
+        //not existing, we initialize it
+        current_agg = {
+            pk: newPun.chan_yymm,
+            authors: {
+                [author]: {
+                    avg: 0,
+                    punsScores: {}
+                }
+            }
+        };
+    }
+    // updates pun score
+    current_agg.authors[author].punsScores[punId] = score;
+    // updates avg
+    current_agg.authors[author].avg = compute_score(current_agg.authors[author]);
+    return current_agg;
+}
+
+// {
+//        pk: chan
+//        authors : {
+//          author_id:{
+//            avg:number,
+//            yymm: { yymm:number }
+//            }
+//        }
+//      }
+function upEver(life_agg, monthlyAgg, pun) {
+    const [chan,yymm] = monthlyAgg.pk.split(':');
+    const [_,author] = pun.pk.split(':');
+    if (!! monthlyAgg.authors[author]){
+        const score = monthlyAgg.authors[author].avg;
+        if (!life_agg) {
+            //not existing, we initialize it
+            life_agg = {
+                pk: chan,
+                authors: {
+                    [author]: {
+                        avg: 0,
+                        yymm: {}
+                    }
+                }
+            };
+        }
+        // updates pun score
+        life_agg.authors[author].yymm[yymm] = score;
+        // updates avg
+        life_agg.authors[author].avg = compute_score(life_agg.authors[author],'yymm');
+    } else {
+        delete life_agg.authors[author];
+    }
+    return life_agg;
+}
+
+
+
+
+function rmMonthly(current_agg, deleted_pun) {
+    const [chan, author] = deleted_pun.pk.split(':');
+    const punId = deleted_pun.sk;
+    delete current_agg.authors[author].punsScores[punId];
+    if (Object.keys(current_agg.authors[author].punsScores) <= 0) {
+        //doesn't have any more puns, let's remove him
+        delete current_agg.authors[author];
+    }
+    else {
+        //still has puns to score
+        current_agg.authors[author].avg = compute_score(current_agg.authors[author]);
+    }
+    log(current_agg);
+    return current_agg;
+}
+
+
+
 
 exports.compute_score = compute_score;
 exports.newItemLens = newItemLens;
 exports.oldItemLens = oldItemLens;
 exports.getNew = getNew;
 exports.getOld = getOld;
+exports.upMonthly = upMonthly;
+exports.rmMonthly = rmMonthly;
+exports.upEver = upEver;
--- punsmonthly/index.test.js
+++ punsmonthly/index.test.js
@@ -1,4 +1,4 @@
-const {compute_score,getNew,getOld} = require('./index');
+const {compute_score,getNew,getOld,upMonthly,upEver,rmMonthly } = require('./index');
 
 test('computes averages correctly', () => {
   wrap = x=> ({punsScores:x})
@@ -7,6 +7,8 @@ test('computes averages correctly', () => {
   expect(compute_score(wrap({a:1,b:0}))).toBeCloseTo(0.5);
   expect(compute_score(wrap({a:1,b:0,c:0}))).toBeCloseTo(0.33);
   expect(compute_score(wrap({}))).toBe(NaN);
+  expect(compute_score({yymm:{a:1,b:2}},'yymm')).toBeCloseTo(1.5);
+  expect(compute_score({otherk:{a:1,b:2}},'otherk')).toBeCloseTo(1.5);
 });
 
 
@@ -48,4 +50,312 @@ test('lenstractors',()=> {
   }
   expect(getNew(record)).toEqual({author:"rob",score:5});
   expect(getOld(record)).toEqual({author:"bob",score:101});
+})
+
+
+test('upMonthly returns a clean record on new pun', ()=>{
+    // it returns new record on not existing
+    const newP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p123',
+        chan_yymm:'c123:2020-01'
+    }
+    const expected = {
+        pk: 'c123:2020-01',
+        authors:{
+            a123:{
+                avg:10,
+                punsScores:{p123:10}
+            }
+        }
+    }
+    expect(upMonthly(undefined,newP)).toEqual(expected)
+})
+
+test('upMonthly appends and update a record with the new pun', ()=>{
+    // it returns new record on not existing
+    const newP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p2',
+        chan_yymm:'c123:2020-01'
+    }
+    const current = {
+        pk: 'c123:2020-01',
+        authors:{
+            a123:{
+                avg:2,
+                punsScores:{p1:2}
+            }
+        }
+    }
+    const expected = {
+        pk: 'c123:2020-01',
+        authors:{
+            a123:{
+                avg:6,
+                punsScores:{p1:2,p2:10}
+            }
+        }
+    }
+    const computed = upMonthly(current,newP)
+    expect(computed.authors.a123.avg).toBeCloseTo(6);
+    expect(computed.authors.a123.punsScores).toEqual(expected.authors.a123.punsScores)
+})
+
+
+test('rmMonthly deletes a pun score among many',()=>{
+    const oldP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p2',
+        chan_yymm:'c123:2020-01'
+    }
+    
+    const current = {
+        pk: 'c123:2020-01',
+        authors:{
+            a123:{
+                avg:6,
+                punsScores:{p1:2,p2:10}
+            }
+        }
+    }
+
+    const expected = {
+        pk: 'c123:2020-01',
+        authors:{
+            a123:{
+                avg:2,
+                punsScores:{p1:2}
+            }
+        }
+    }
+    
+    const computed = rmMonthly(current,oldP)
+    expect(computed.authors.a123.punsScores).toEqual(expected.authors.a123.punsScores)
+    expect(computed.authors.a123.avg).toBeCloseTo(2);
+})
+
+test('rmMonthly deletes the last pun score and the author',()=>{
+    const oldP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p2',
+        chan_yymm:'c123:2020-01'
+    }
+    
+    const current = {
+        pk: 'c123:2020-01',
+        authors:{
+            a123:{
+                avg:10,
+                punsScores:{p2:10}
+            },
+            a3:{
+                avg:10,
+                punsScores:{p1:10}
+            }
+        }
+    }
+
+    const expected = {
+        pk: 'c123:2020-01',
+        authors:{
+            a3:{
+                avg:10,
+                punsScores:{p1:10}
+            }
+        }
+    }
+    
+    const computed = rmMonthly(current,oldP)
+    expect(computed.authors).toEqual(expected.authors)
+})
+
+
+test('upEver returns a clean record on new pun', ()=>{
+    // it returns new record on not existing
+    const newP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p2',
+        chan_yymm:'c123:2020-01'
+    }
+    
+    const mo = {
+        pk: 'c123:2020-01',
+        authors:{
+            a123:{
+                avg:6,
+                punsScores:{p1:2,p2:10}
+            }
+        }
+    }
+    
+    const expected = {
+        pk: 'c123',
+        authors:{
+            a123:{
+                avg:6,
+                yymm:{'2020-01':6}
+            }
+        }
+    }
+    expect(upEver(undefined, mo, newP)).toEqual(expected)
+})
+
+
+
+
+test('upEver updates record correctly', ()=>{
+    // it returns new record on not existing
+    const newP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p6',
+        chan_yymm:'c123:2020-01'
+    }
+    
+    const mo = {
+        pk: 'c123:2019-12',
+        authors:{
+            a123:{
+                avg:3,
+                punsScores:{p3:0,p2:6}
+            }
+        }
+    }
+    
+    const current = {
+        pk: 'c123',
+        authors:{
+            a123:{
+                avg:6,
+                yymm:{'2020-01':6}
+            }
+        }
+    }
+
+    const expected = {
+        pk: 'c123',
+        authors:{
+            a123:{
+                avg:4.5,
+                yymm:{'2020-01':6, '2019-12':3}
+            }
+        }
+    }
+    const computed = upEver(current, mo, newP);
+    
+    expect(computed.authors.a123.avg).toBeCloseTo(expected.authors.a123.avg)
+    expect(computed.authors.a123.yymm).toEqual(expected.authors.a123.yymm)
+})
+
+
+
+test('upEver updates record correctly even on deletion of one pun', ()=>{
+    // it returns new record on not existing
+    const rmP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p6',
+        chan_yymm:'c123:2020-01'
+    }
+    
+    const mo = {
+        pk: 'c123:2019-12',
+        authors:{
+            a123:{
+                avg:3,
+                punsScores:{p3:0,p2:6}
+            },
+            a2:{
+                avg:10,
+                punsScores:{p1:10}
+            }
+        }
+    }
+    
+    const current = {
+        pk: 'c123',
+        authors:{
+            a123:{
+                avg:8,
+                yymm:{'2019-12':8}
+            },
+            a2:{
+                avg:10,
+                yymm:{'2020-01':10}
+            }
+        }
+    }
+
+    const expected = {
+        pk: 'c123',
+        authors:{
+            a123:{
+                avg:3,
+                yymm:{'2019-12':3}
+            },
+            a2:{
+                avg:10,
+                yymm:{'2020-01':10}
+            }
+        }
+    }
+    const computed = upEver(current, mo, rmP);
+    
+    expect(computed.authors.a123.avg).toBeCloseTo(expected.authors.a123.avg)
+    expect(computed.authors.a123.yymm).toEqual(expected.authors.a123.yymm)
+})
+
+
+
+test('upEver updates record correctly even on deletion of a month', ()=>{
+    // it returns new record on not existing
+    const rmP = {
+        pk:'c123:a123',
+        score:10,
+        sk:'p6',
+        chan_yymm:'c123:2020-01'
+    }
+    
+    const mo = {
+        pk: 'c123:2019-12',
+        authors:{
+            a2:{
+                avg:10,
+                punsScores:{p1:10}
+            }
+        }
+    }
+    
+    const current = {
+        pk: 'c123',
+        authors:{
+            a123:{
+                avg:8,
+                yymm:{'2019-12':8}
+            },
+            a2:{
+                avg:10,
+                yymm:{'2020-01':10}
+            }
+        }
+    }
+
+    const expected = {
+        pk: 'c123',
+        authors:{
+            a2:{
+                avg:10,
+                yymm:{'2020-01':10}
+            }
+        }
+    }
+    const computed = upEver(current, mo, rmP);
+    
+    expect(computed.authors).toEqual(expected.authors)
 })
